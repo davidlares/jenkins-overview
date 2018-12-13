9ad92544abd1195e4650c1dc650467ff7dd5bd9a
David Lares <david.e.lares@gmail.com>
2018-12-13 17:37:50 -0400

    Test GH/J Communication

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main {
   public static void main(String[] args){
     System.out.println("Hello, Jenkins from Java");
+    System.out.println("Test Message: GH - Jenkins Webhook communication");
   }
 }
